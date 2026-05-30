d65c79b8b01a2e065fb998910cc7044e3d957147
jsy <jisy@yczcjk.com>
2026-05-30 16:27:11 +0800

    在终端中可以用python直接执行文件

---
+++ imports.py
@@ -0,0 +1,8 @@
+"""
+@Author  : Jsy
+@Date    : 2026/5/30 13:41
+@Description : 在终端中可以用python直接执行文件
+"""
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
